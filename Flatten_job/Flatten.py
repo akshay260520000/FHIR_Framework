63e820477c5ad5667e4f65631b25bcbb58568be8
Akshay Mastud <68811651+akshay260520000@users.noreply.github.com>
2023-02-17 12:58:43 +0000

    commit

--- Flatten_job/Flatten.py
+++ Flatten_job/Flatten.py
@@ -13,10 +13,14 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 with open('/workspaces/FHIR_Framework/Config_files/hcs_config.json') as config_file:
-    config=json.load(config_file)["Source_Schema"]
+    con=json.load(config_file)
+
+schema_temp = con["Source_Schema"]
+schema = StructType.fromJson(schema_temp)
 
 
-schema = StructType.fromJson(config)
+
+
 
 input_path ='/workspaces/FHIR_Framework/Input_data_files/hcs_json.json'
 
@@ -25,15 +29,11 @@ hcs_df.printSchema()
 hcs_df.show(truncate=False)
 
 print('---------------------Target Schema-----------------')
-with open('/workspaces/FHIR_Framework/Config_files/hcs_config.json') as config_file:
-    targetschema=json.load(config_file)["targetschema"]
+targetschema = con["targetschema"]
 targetschema_df = hcs_df.select(*(targetschema))
 targetschema_df.show(truncate=False)
 
 print("-------------------------Explode Level 1------------------")
-with open('/workspaces/FHIR_Framework/Config_files/hcs_config.json') as config_file:
-    explode1=json.load(config_file)["explode_1"]
-
-
+explode1 = con["explode_1"]
 explode1_df = targetschema_df.withColumn(f"new_{explode1}",explode_outer(explode1)).drop(col(f"{explode1}"))
 explode1_df.show(truncate=False)
