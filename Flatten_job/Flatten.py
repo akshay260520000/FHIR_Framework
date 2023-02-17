e20dc8890ba5cf9eb36c4081554df6f97a70de31
Keshav.J.Bajaj <keshavbajaj4444@gmail.com>
2023-02-17 10:32:39 +0000

    commit

--- Flatten_job/Flatten.py
+++ Flatten_job/Flatten.py
@@ -1,36 +1,42 @@
 import sys
 import pyspark
+from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.sql.types import *
 import json
 from pyspark.sql.functions import explode, explode_outer, col, when, split,  \
     concat_ws, collect_set, udf, element_at, to_date, lit, desc, dense_rank
-sc = SparkContext()
+
+spark = SparkSession.builder \
+    .master("local[1]") \
+    .appName("PySpark Read JSON") \
+    .getOrCreate()
 
 
 
 with open('/workspaces/codespaces-blank/Config_files/hcs_config.json') as config_file:
-    config = json.loads(config_file)
+    config=json.load(config_file)["Source_Schema"]
 
-
-schema = config["schema"]
-schema = StructType.fromJson(schema)
-targetschema = config["targetschema"]
+# config=config.decode('utf-8')
+# config=json.loads(config)
+# schema = config["schema"]
+schema = StructType.fromJson(config)
+# targetschema = config["targetschema"]
 
 
 input_path ='/workspaces/codespaces-blank/Input_data_files/hcs_json.json'
 
 
-hcs_df = sc.read.schema(schema).format("json").option("multiLine", "true").load(input_path)
+hcs_df = spark.read.schema(schema).format("json").option("multiLine", "true").load(input_path)
 hcs_df.printSchema()
 hcs_df.show(truncate=False)
 
-print('---------------------Target Schema-----------------')
-targetschema_df = hcs_df.select(*(targetschema))
-targetschema_df.show(truncate=False)
+# print('---------------------Target Schema-----------------')
+# targetschema_df = hcs_df.select(*(targetschema))
+# targetschema_df.show(truncate=False)
 
-print("-------------------------Explode Level 1------------------")
-explode1 = config["explode_1"]
-explode1_df = targetschema_df.withColumn("{str(explode1)}",explode_outer(explode1)).drop(col("{str(explode1)}"))
-explode1_df.show(truncate=False)
+# print("-------------------------Explode Level 1------------------")
+# explode1 = config["explode_1"]
+# explode1_df = targetschema_df.withColumn("{str(explode1)}",explode_outer(explode1)).drop(col("{str(explode1)}"))
+# explode1_df.show(truncate=False)
 
