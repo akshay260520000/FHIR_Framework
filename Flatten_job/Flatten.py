6aa7aee40feb1cef4b099e3feeb0cb860bc1550a
Keshav.J.Bajaj <keshavbajaj4444@gmail.com>
2023-02-27 12:12:28 +0000

    commit

--- Flatten_job/Flatten.py
+++ Flatten_job/Flatten.py
@@ -13,16 +13,19 @@ spark = SparkSession.builder \
     .appName("PySpark Read JSON") \
     .getOrCreate()
 
-with open('/workspaces/FHIR_Framework/Config_files/org_config.json') as config_file:
+with open('/workspaces/FHIR_Framework/Config_files/practioner_config.json') as config_file:
     con=json.load(config_file)
 
 schema_temp = con["Source_Schema"]
 schema = StructType.fromJson(schema_temp)
 
-input_path ='/workspaces/FHIR_Framework/Input_data_files/org_json.json'
+input_path ='/workspaces/FHIR_Framework/Input_data_files/practioner_json.json'
 
 hcs_df = spark.read.schema(schema).format("json").option("multiLine", "true").load(input_path)
-hcs_df.printSchema()
+
+# def changename(hcs_df,colum):
+#     hcs_df.select("*",colum)
+
 
 
 print('---------------------Target Schema-----------------')
@@ -31,13 +34,11 @@ explode_dict = con["explodedict"]
 
 for i in range(1,(len(targetschema_dict)+1)):
     
-    hcs_df =hcs_df.select("*",*(targetschema_dict[f'{i}']))
-    
-    
-    
+    hcs_df=hcs_df.select("*",*(targetschema_dict[f'{i}']))            
     if(i<=len(explode_dict)):
         for column in explode_dict[f'{i}']:
             hcs_df = hcs_df.withColumn(f"new_{column}",explode_outer(column)).\
                 drop(col(f"{column}")).withColumnRenamed(f'new_{column}',f'{column}')
 print("______Final_________")
 hcs_df.show(5)
+
--- Flatten_job/trialthings.py
+++ Flatten_job/trialthings.py
@@ -13,16 +13,15 @@ spark = SparkSession.builder \
     .appName("PySpark Read JSON") \
     .getOrCreate()
 
-with open('/workspaces/FHIR_Framework/Config_files/org_config.json') as config_file:
+with open('/workspaces/FHIR_Framework/Config_files/hcs_config.json') as config_file:
     con=json.load(config_file)
 
 schema_temp = con["Source_Schema"]
 schema = StructType.fromJson(schema_temp)
 
-input_path ='/workspaces/FHIR_Framework/Input_data_files/org_json.json'
+input_path ='/workspaces/FHIR_Framework/Input_data_files/hcs_json.json'
 
 hcs_df = spark.read.schema(schema).format("json").option("multiLine", "true").load(input_path)
-hcs_df.printSchema()
 
 
 print('---------------------Target Schema-----------------')
@@ -32,12 +31,24 @@ explode_dict = con["explodedict"]
 for i in range(1,(len(targetschema_dict)+1)):
     
     hcs_df =hcs_df.select("*",*(targetschema_dict[f'{i}']))
-    
-    
-    
+
+                
     if(i<=len(explode_dict)):
         for column in explode_dict[f'{i}']:
             hcs_df = hcs_df.withColumn(f"new_{column}",explode_outer(column)).\
                 drop(col(f"{column}")).withColumnRenamed(f'new_{column}',f'{column}')
 print("______Final_________")
 hcs_df.show(5)
+hcs_df.printSchema()
+
+## Things to do
+
+## DO manual testing of json data and also try to explode manually
+
+## solve that __ problem..
+
+## Alter the dictionaries.abs
+
+## Deine the target schema 
+
+## Make code resource name free..
